5637607ff0eb847acf20f7ce0685ffffd6f78eb1
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2019-03-29 02:02:01 -0300

    add base scaffolding for test #2

--- test-questions-search/src/app/app-routing.module.ts
+++ test-questions-search/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './views/questions/not-found/not-found.component';
+import { QuestionDetailComponent } from './views/questions/question-detail/question-detail.component';
+import { QuestionListComponent } from './views/questions/question-list/question-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'questions', component: QuestionListComponent },
+  { path: 'questions/:id', component: QuestionDetailComponent },
+  { path: '', redirectTo: '/questions', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
